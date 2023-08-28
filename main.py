695de97918251f43893840912a6041bf6071580f
BersUmar <umar-03@inbox.ru>
2023-08-28 15:24:05 +0300

    temporary code

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from src.utils import sort, get_hh_vacancy, get_sj_vacancy, top
 
 
 def main():
-    # keyword = input("Введите ключевое слово для поиска вакансий: ")
-    keyword = 'python'
+    keyword = input("Введите ключевое слово для поиска вакансий: ")
+    # keyword = 'python'
     hh_engine = HH(keyword)
     sj_engine = Superjob(keyword)
     hh_connector = Connector("vacancy_hh.json")
@@ -20,7 +20,7 @@ def main():
         if page < hh_pages:
             hh_engine.params["page"] = page
             page += 1
-            hh_vacancies = json.dumps(hh_engine.get_request())
+            hh_vacancies = hh_engine.get_request().json()
             hh_pages = hh_vacancies["pages"]
             hh_items = hh_vacancies["items"]
             hh_connector.insert(hh_items)
@@ -29,7 +29,7 @@ def main():
 
         if more:
             sj_engine.params["page"] = sj_engine.params["page"] + 1
-            sj_vacancies = json.dumps(sj_engine.get_request())
+            sj_vacancies = sj_engine.get_request().json()
             sj_items = sj_vacancies["objects"]
             more = sj_vacancies["more"]
             sj_connector.insert(sj_items)
